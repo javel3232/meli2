9ebea16fc30904c980ec8edf52c32c23432cc129
javel3232 <jaglervel97@gmail.com>
2023-05-01 22:50:42 -0500

    se modifica las iamagenes

--- components/Descubre.tsx
+++ components/Descubre.tsx
@@ -7,7 +7,7 @@ interface DescubreProps {
 }
 const Descubre = ({imagedes,titulo,titulo2,texto}:DescubreProps) => {
     return (
-        <div className="flex items-center bg-white shadow-sm rounded-md h-[250px] w-[584px] gap-[32.16px]">
+        <div className="flex items-center justify-between bg-white shadow-sm rounded-md h-[250px] w-[584px] gap-[16px]">
             <div className="flex justify-center flex-col gap-[11px] pl-[32px]">
 
                 <div className="sub">
@@ -20,7 +20,7 @@ const Descubre = ({imagedes,titulo,titulo2,texto}:DescubreProps) => {
 
                 <input className="w-[96px] h-[36px]  bg-blue-500 rounded-md border-none text-white" type="button" value="Ver más" />
             </div>
-            <div className="h-[250px] w-[290px] pl-[19px]  border-0 rounded-0">
+            <div className="flex justify-end  h-[250px] w-[290px] pl-[19px]  rounded-0">
                 <img src={imagedes} alt="" />
             </div>
 
